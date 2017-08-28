61a9940c6b0fe2f292ae5ae6a5f2bec6cd7128c8
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-28 14:50:47 -0700

    .php vs .html

--- embed.php
+++ embed.php
@@ -1,3 +1,5 @@
+<!-- when you have .php, you're telling apache that this file may have some php in it -->
+
 <?php
 
   php code here;
@@ -6,7 +8,7 @@
 
   ?>
 
-
+<!-- this works everywhere -->
 <? php ?>
 
 <!-- this is php shorthand, but it's bad form because not all servers have php shorthand activated; you would have to activate it manually  -->
