1725c565f706e3b6310955dfaca2f89c153825ff
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-28 14:57:19 -0700

    strings in PHP

--- embed.php
+++ embed.php
@@ -1,20 +1,2 @@
-<!-- when you have .php, you're telling apache that this file may have some php in it -->
-
-<?php
-
-  php code here;
-
-  php code here;
-
-  ?>
-
-<!-- this works everywhere -->
-<? php ?>
-
-<!-- this is php shorthand, but it's bad form because not all servers have php shorthand activated; you would have to activate it manually  -->
-
-<? ?>
-
-<?= ?>
-
-<% %> </%>
+<!-- this is how we write strings in PHP. echo is a function that prints it onto the screen -->
+<?php echo "hello student"; ?>
