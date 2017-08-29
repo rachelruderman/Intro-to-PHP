b2137c2396f6a262c17235be64bc1614aace8a7e
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-28 18:21:25 -0700

    concat with .

---
+++ variables.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $name = "Rachel";
+      $number = 100;
+//concatanation in php
+      echo $name . $number;
+    ?>
+  </body>
+</html>
