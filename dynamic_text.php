2b2aafb986990a297210029f1c4f0db4780df2f9
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-28 17:50:03 -0700

    variables in php

---
+++ dynamic_text.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $title = 'Rachel'
+    ?>
+
+    <h1>
+      <?php echo $title ?>
+    </h1>
+  </body>
+</html>
