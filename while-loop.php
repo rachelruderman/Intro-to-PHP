90d23e55aa78b1fde66e65ee3662c7501eeb02a3
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-23 12:16:36 -0700

    while loops

---
+++ while-loop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>While Loops</title>
+  </head>
+  <body>
+    <?php
+    $counter = 0;
+    while ($counter < 10){
+      echo "Hello Students" . "<br>";
+      $counter++;
+    }
+    ?>
+  </body>
+</html>
