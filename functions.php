d242904054444c7318d83f3c76548919678c7535
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-23 15:33:03 -0700

    binding variables to global scope

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <?php
-
+      //returning values from functions
       function addNumbers($num1, $num2){
         $sum = $num1 + $num2;
         return $sum;
@@ -14,6 +14,25 @@
 
       $result = addNumbers(345, 100);
       echo $result;
+
+      //accessing global scope in PHP
+      $x = 'outside';
+
+      function convert(){
+        global $x; //make PHP recognize that we're using the global scope. Now we can do whatever we want to this variable
+        $x = 'inside'; //local
+      }
+
+      function convertIt(){
+        global $x;
+        $x = 'outtie'; //local
+      }
+
+      echo $x;
+      convert();
+      echo $x;
+      convertIt();
+      echo $x;
 
     ?>
   </body>
