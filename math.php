1664d4c8b8b5e217f133936bcf8a327b406ebe5f
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-23 10:50:50 -0700

    math in PHP

---
+++ math.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<?php
+
+  echo 56 + 45;
+  echo "<br/>";
+  echo 56 + 45;
+  $number1 = 12;
+  $number2 = 14;
+  echo "<br/>";
+  echo $number1 + $number2;
+  
+  ?>
+  </body>
+</html>
