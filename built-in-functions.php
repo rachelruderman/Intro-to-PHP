9263ed8891d8c6a42d1598bbabf51c925da73be8
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-23 16:10:35 -0700

    built-in functions

---
+++ built-in-functions.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo pow(2, 7);   //two to the 7th power
+      echo rand();      //returns a random number every time
+      echo sqrt(100);   //square root
+      echo ceil(4.6);   //round up
+      echo floor(4.6);  //round down
+      echo strlen('hello'); //character count
+      echo strtoupper('hello'); //put in all caps
+      echo strtolower('HELLO'); //to lowercase
+
+      //pretty much any type of array function you could want is already built in - including sort, max, min
+
+    ?>
+  </body>
+</html>
