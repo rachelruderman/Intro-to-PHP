c396bea273a034f4ba39093422f1704ca81a93b4
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-23 19:58:18 -0700

    form validation

--- form.php
+++ form.php
@@ -1,10 +1,25 @@
 <?php
   if(isset($_POST['submit'])){
-    echo 'yesss it works';
+    $name = array('Edwin', 'Student', 'Peter', 'Samid');
+    $minimum = 5;
+    $maximum = 10;
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if(strlen($username) < $minimum){
+      echo "Username has to be longer than $minimum";
+    }
+
+    //in_array is case sensitive
+    if(!in_array($username, $name)){
+      echo 'Sorry, you are not allowed';
+    } else {
+      echo 'Welcome!';
+    }
+
+    echo $username . '<br>' . $password;
   }
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  echo $username . '<br>' . $password;
 ?>
 
 <!DOCTYPE html>
