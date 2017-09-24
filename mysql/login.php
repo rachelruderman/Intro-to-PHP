5dbf965451719ac0591fab377d226908834dad93
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-23 20:58:04 -0700

    create php form with bootstrap

---
+++ mysql/login.php
@@ -0,0 +1,36 @@
+<?php
+  if(isset($_POST['submit'])){
+    echo 'yes we got it!';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    echo '<br>' . "hi $username, your password is $password";
+  }
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+  </head>
+  <body>
+    <div class='container'>
+      <div class='col-xs-6'>
+        <form class="" action="login.php" method="post">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input class='form-control' type="text" name="username" value="">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input class='form-control' type="password" name="password" value="">
+          </div>
+            <input name='submit' type="submit" name="submit" value="Submit" class='btn btn-primary'>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
