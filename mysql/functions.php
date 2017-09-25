61c0360aa6ee285bd02deeaa65bb3a432a4dc141
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-25 13:44:23 -0700

    password encryption

--- mysql/functions.php
+++ mysql/functions.php
@@ -54,6 +54,12 @@
     $username = mysqli_real_escape_string($connection, $username);
     $password = mysqli_real_escape_string($connection, $password);
 
+    $hashFormat = "$2y$10$";
+    $salt = "iusesomecrazystrings22";
+    $hash_and_salt = $hashFormat . $salt;
+    
+    $password = crypt($password, $hash_and_salt);
+
     $query = "INSERT INTO users(username, password) ";
     $query .= "VALUES ('$username', '$password')";
 
