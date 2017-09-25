80e655e60de5a01254e5f8fe1195ee2c5e1c1469
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-25 15:15:59 -0700

    calling object properties/methods

--- class_car.php
+++ class_car.php
@@ -1,8 +1,14 @@
 <?php
 //classes are blueprints
   class Car {
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+
     function wheels(){
       echo "spin the wheels";
+      $this->wheels = 10;
     }
   }
 
@@ -21,8 +27,15 @@
   $bmw      = new Car();
   $mercedes = new Car();
 
-  //call method on an object
+  //call properties on an object with ->
+  //no dollar sign when calling object properties
+  echo $bmw->wheels;
   $bmw->wheels();
   $mercedes->wheels();
+  echo $bmw->hood;
+  echo $bmw->wheels;
+  echo $mercedes->wheels;
+
+  //$this is how we reference a class inside a class
 
  ?>
